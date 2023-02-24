bb1354edefa665a0ab8983b81914b55e2b105be8
marimo_7 <120710648+nana-chan7@users.noreply.github.com>
2023-02-25 00:52:16 +0900

    3

--- todoApp/todo/models.py
+++ todoApp/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    memo = models.TimeField()   # Todoの内容を保持するフィールド(テキスト)
+    is_done = models.BooleanField(default=False)    # 処理済みかどうかを管理する(True か False)
+    created_at = models.DateTimeField(auto_now_add=True)   # 作成日時を保持(日付型)
+    updated_at = models.DateTimeField(auto_now=True)    # 更新日時を保持(日付型)
+    
